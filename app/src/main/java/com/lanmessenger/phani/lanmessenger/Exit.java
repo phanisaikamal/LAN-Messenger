e24d0fbab42fb02fae4d6d9347438a70c4a2ab83
phanisaikamal <lingamsai3@gmail.com>
2017-04-01 19:56:17 +0530

    Add files via upload

---
+++ app/src/main/java/com/lanmessenger/phani/lanmessenger/Exit.java
@@ -0,0 +1,10 @@
+package com.lanmessenger.phani.lanmessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Exit extends Activity {
+}
